fe857fd098f6b040be726bc4b8b336cf762c1743
Elisson Braz <59119081+ElissonBraz@users.noreply.github.com>
2025-08-24 20:00:06 -0300

    Commit
    Modificação nomes projeto, pacote e classe

--- sistema processo seletivo/src/candidatura/ProcessoSeletivo.java
+++ sistema processo seletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,17 +1,16 @@
-package cases;
+package candidatura;
 
 import java.util.Scanner;
 
 /* Case 1:
  Vamos imaginar que em um processo seletivo existe o valor base salarial de R$ 2.000,00 e o salário pretendido
  pelo candidato. Vamos elaborar um controle de fluxo onde:
-
   * Se o valor salario base for maior que valor salario pretendido, imprima LIGAR PARA O CANDIDATO;
   * Senão Se o valor salario base for igual ao valor salario pretendido, imprima: LIGAR PARA O CANDIDATO COM
    CONTRA PROPOSTA;
   * Senão imprima: AGUARDANDO RESULTADO DEMAIS CANDIDATOS.
  */
-public class Case1 {
+public class ProcessoSeletivo {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
